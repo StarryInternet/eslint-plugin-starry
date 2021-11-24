45d0800adc78648bc9f461b97cac94becb92ddc3
Nicholas Bonetto <nbonetto@starry.com>
2021-11-23 16:23:13 -0800

    Add Rule for No Invisible Characters
    Disallows invisible characters in source code text that aren't already
    illegal characters.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'computed-property-spacing': rule('computed-property-spacing'),
     'space-in-parens': rule('space-in-parens'),
-    'aligned-requires': rule('aligned-requires')
+    'aligned-requires': rule('aligned-requires'),
+    'no-invisible-characters': rule('no-invisible-characters')
   }
 };
--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,4 @@
 require('./computed-property-spacing'),
 require('./space-in-parens'),
 require('./aligned-requires');
+require('./no-invisible-characters');
